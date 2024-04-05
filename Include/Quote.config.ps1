e32344e5f7dac68fc0dc316f13786432721fbaab
Krystian Maksymowicz <krystian.maksymowicz@gmail.com>
2024-04-05 12:57:29 +0200

    Fix last commit

--- Include/Quote.config.ps1
+++ Include/Quote.config.ps1
@@ -5,9 +5,11 @@ $Quotes = @(
                 "Jednak nawet niemoralne zwycięstwo musi przewyższać moralną porażkę.",
                 "Zwycięzca będzie miał szansę odpokutować, jeśli sumienie tego zażąda.",
                 "Pokonani tracą taką możliwość."
-    }
+    },
+    @{
         Author = "Harlan Ellison"
         Quote = "Nie masz prawa do swojej opinii.",
                 "Masz prawo do świadomej opinii.",
                 "Nikt nie ma prawa być ignorantem."
+    }
 )
