00171175691785452a2cd197b7747dbee1f8feb9
Krystian Maksymowicz <krystian.maksymowicz@gmail.com>
2024-04-07 08:56:44 +0200

    Added Herbert Marcuse - Represive Tolerance quote. Added language tag for future feature.

--- Include/Quote.config.ps1
+++ Include/Quote.config.ps1
@@ -1,6 +1,7 @@
 $Quotes = @(
     @{
         Author = "Immanuel Kant"
+        Language = "Polish"
         Quote = "Konieczność przewyższa moralność. Nie bez żalu. Nie bez wstydu.",
                 "Jednak nawet niemoralne zwycięstwo musi przewyższać moralną porażkę.",
                 "Zwycięzca będzie miał szansę odpokutować, jeśli sumienie tego zażąda.",
@@ -8,8 +9,16 @@ $Quotes = @(
     },
     @{
         Author = "Harlan Ellison"
+        Language = "Polish"
         Quote = "Nie masz prawa do swojej opinii.",
                 "Masz prawo do świadomej opinii.",
                 "Nikt nie ma prawa być ignorantem."
+    },
+    @{
+        Author = "Herbert Marcuse", "Represive Tolerance"
+        Language = "English"
+        Quote = "Librerating tolerance, then, would mean intolerance",
+                "against movements from the Right and tolerance of",
+                "movements from the Left."
     }
 )
