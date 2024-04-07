1557d8c19437fd30a84ac78e94180ad833d528a4
Krystian Maksymowicz <krystian.maksymowicz@gmail.com>
2024-04-07 09:05:35 +0200

    Added feature: Language picking

--- Public/Get-Qotd.ps1
+++ Public/Get-Qotd.ps1
@@ -1,11 +1,18 @@
 function Get-Qotd {
     [Alias('Get-Quote')]
     [CmdletBinding()]
-    param ()
+    param (
+        [Parameter()]
+        [string[]]
+        $Language
+    )
     
     begin {}
     
     process {
+        if ($Language) {
+            $Quotes = $Quotes.Where({$_.Language -in $Language})
+        }
         [Quote]::new((Get-Random -InputObject $Quotes)).ToString()
     }
     
