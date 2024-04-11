5de842817895b45580d3839d99ae27d17562e5d9
Krystian Maksymowicz <krystian.maksymowicz@gmail.com>
2024-04-11 08:24:32 +0200

    Added some quotes

--- Include/Quote.config.ps1
+++ Include/Quote.config.ps1
@@ -8,6 +8,52 @@ $Quotes = @(
                 "Pokonani tracą taką możliwość."
     },
     @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "One who makes himself a worm cannot complain",
+                "afterwards if people step on him."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "Genius is the ability to independently arrive at and",
+                "understand concepts that would normally have to be",
+                "taught by another person."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "For peace to reign on Earth, humans must evolve into",
+                "new beings who have learned to see the whole first."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "Morality is not properly the doctrine of how we may",
+                "make ourselves happy, but how we may make",
+                "ourselves worthy of happiness."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "Seek not the favor of the multitude; it is seldom got by",
+                "honest and lawful means. But seek the testimony of",
+                "few; and number not voices, but weigh them."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "All false art, all vain wisdom, lasts its time but finally",
+                "destroys itself, and its highest culture is also the epoch",
+                "of its decay."
+    },
+    @{
+        Author = "Immanuel Kant"
+        Language = "English"
+        Quote = "May you live your life as if the maxim of your actions",
+                " were to become universal law."
+    },
+    @{
         Author = "Harlan Ellison"
         Language = "Polish"
         Quote = "Nie masz prawa do swojej opinii.",
@@ -15,7 +61,7 @@ $Quotes = @(
                 "Nikt nie ma prawa być ignorantem."
     },
     @{
-        Author = "Herbert Marcuse", "Represive Tolerance"
+        Author = "Herbert Marcuse", '„Represive Tolerance”'
         Language = "English"
         Quote = "Librerating tolerance, then, would mean intolerance",
                 "against movements from the Right and tolerance of",
